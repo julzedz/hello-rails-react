47d050d732eb3cc565af40f9558b8ae237bfe099
julzedz <edoziechukwuma@gmail.com>
2023-08-16 18:09:38 +0100

    Run linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    random_greeting = Message.order("RANDOM()").first
+    random_greeting = Message.order('RANDOM()').first
     render json: random_greeting
   end
 end
